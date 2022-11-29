9b14c2468440a967cfc6e3c0b5cc1d55a82f371c
garzonsergio <scgarzonp@gmail.com>
2022-11-29 16:12:13 -0500

    There was included the SearchBar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { SearchBar } from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        {/* Componente de React */}
+        <SearchBar title={"Google Imágenes"} />
+        {/* Se envían las props a través title */}
+
+
+        {/* alt, src, className son atributos del ELEMENTO img */}
         <img src={logo} className="App-logo" alt="logo" />
+
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
