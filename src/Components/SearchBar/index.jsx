eeda42d355803834b4b4bbea802ed47d18f721f0
garzonsergio <scgarzonp@gmail.com>
2022-12-01 09:31:52 -0500

    Minimun components created

---
+++ src/Components/SearchBar/index.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import axios from "axios";
+
+function SearchBar(props) {
+
+  
+
+
+
+  return (
+    <div>
+        <h1>{props.title}</h1>        
+    </div>
+  )
+}
+
+export {SearchBar}
