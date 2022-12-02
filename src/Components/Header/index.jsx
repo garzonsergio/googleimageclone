888b6be13beda866c8ccf127c5a8230a8e524676
garzonsergio <scgarzonp@gmail.com>
2022-12-01 23:33:12 -0500

    The google logo and main input has been located in the final position

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import "./style.css"
-import { HeaderSearchBar } from "../HeaderSearchBar";
+// import { HeaderSearchBar } from "../HeaderSearchBar";
 
 function Header() {
   return (
     <React.Fragment>
       <header>
-        <HeaderSearchBar />
+        {/* <HeaderSearchBar /> */} 
         <nav>
-          <ul className="nav-bar">
-            {/* <li className="gmail" >
-              Gmail
-            </li>
-            <li className="imagenes">
-              Imágenes
-            </li> */}
+          <ul className="nav-bar">          
             <li className="products">
               <svg className="gb_Pe" focusable="false" viewBox="0 0 24 24">
                 <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
